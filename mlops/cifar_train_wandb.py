d28f3731a7e541885ea711ed45b426827fbc1476
Gauri Chaware <92186637+33018945@users.noreply.github.com>
2022-12-03 00:34:58 -0500

    Commented unwanted helper functions/classes
    Also added comment on changing API Key to the existing users

--- mlops/cifar_train_wandb.py
+++ mlops/cifar_train_wandb.py
@@ -19,48 +19,48 @@ from cifar10.module import Module
 import warnings
 import wandb
 import os
-os.environ['WANDB_API_KEY'] = '2b5e640eebb24c5d55d40a699278c9b1a24eea8a'
+os.environ['WANDB_API_KEY'] = '2b5e640eebb24c5d55d40a699278c9b1a24eea8a' #add your account's API key
 wandb.login()
 
 warnings.filterwarnings("ignore")
 
 utils.set_seed(0)
 
-class CIFAR10DataModule(pl.LightningDataModule):
-    def __init__(self, batch_size, data_dir: str = './'):
-        super().__init__()
-        self.data_dir = data_dir
-        self.batch_size = batch_size
+# class CIFAR10DataModule(pl.LightningDataModule):
+#     def __init__(self, batch_size, data_dir: str = './'):
+#         super().__init__()
+#         self.data_dir = data_dir
+#         self.batch_size = batch_size
 
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+#         self.transform = transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+#         ])
         
-        self.num_classes = 10
+#         self.num_classes = 10
     
-    def prepare_data(self):
-        CIFAR10(self.data_dir, train=True, download=True)
-        CIFAR10(self.data_dir, train=False, download=True)
+#     def prepare_data(self):
+#         CIFAR10(self.data_dir, train=True, download=True)
+#         CIFAR10(self.data_dir, train=False, download=True)
     
-    def setup(self, stage=None):
-        # Assign train/val datasets for use in dataloaders
-        if stage == 'fit' or stage is None:
-            cifar_full = CIFAR10(self.data_dir, train=True, transform=self.transform)
-            self.cifar_train, self.cifar_val = random_split(cifar_full, [45000, 5000])
+#     def setup(self, stage=None):
+#         # Assign train/val datasets for use in dataloaders
+#         if stage == 'fit' or stage is None:
+#             cifar_full = CIFAR10(self.data_dir, train=True, transform=self.transform)
+#             self.cifar_train, self.cifar_val = random_split(cifar_full, [45000, 5000])
 
-        # Assign test dataset for use in dataloader(s)
-        if stage == 'test' or stage is None:
-            self.cifar_test = CIFAR10(self.data_dir, train=False, transform=self.transform)
+#         # Assign test dataset for use in dataloader(s)
+#         if stage == 'test' or stage is None:
+#             self.cifar_test = CIFAR10(self.data_dir, train=False, transform=self.transform)
     
-    # def train_dataloader(self):
-    #     return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
+#     # def train_dataloader(self):
+#     #     return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
 
-    def val_dataloader(self):
-        return DataLoader(self.cifar_val, batch_size=self.batch_size)
+#     def val_dataloader(self):
+#         return DataLoader(self.cifar_val, batch_size=self.batch_size)
 
-    # def test_dataloader(self):
-    #     return DataLoader(self.cifar_test, batch_size=self.batch_size)
+#     # def test_dataloader(self):
+#     #     return DataLoader(self.cifar_test, batch_size=self.batch_size)
         
         
         
