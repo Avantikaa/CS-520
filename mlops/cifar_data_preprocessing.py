484a9d7c4a21f62b30e4735a2f7b328718a195b7
ankitamakker <amakker@umass.edu>
2022-12-03 20:37:37 -0500

    data processing pipeline

---
+++ mlops/cifar_data_preprocessing.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+
+from cifar10.cifar_datamodule import CIFARDataModule
+import mlflow
+import torch
+
+registry_uri = 'sqlite:///mlflow.db'
+tracking_uri = 'http://127.0.0.1:5000'
+
+def download_and_transform_data(args):
+    with mlflow.start_run() as mlrun:
+        cifar10_dm = CIFARDataModule(args)
+        torch.save(cifar10_dm, "cifar10_dm.pkl")
+        mlflow.log_artifact("cifar10_dm.pkl", args.datamodule_path)
+        print("Data extracted, transformed and saved at ", args.datamodule_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument("--dataset-path", default="cifar10/dataset")
+    parser.add_argument("--datamodule-path", default="cifar10/datamodule")
+    parser.add_argument("--batch-size", default=8)
+    args = parser.parse_args()
+    print(args)
+    download_and_transform_data(args)
+    
