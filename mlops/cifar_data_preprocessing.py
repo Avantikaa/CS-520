9001e1dec83c0bc58ffb2787d16479d7dd062dfd
ankitamakker <amakker@umass.edu>
2022-12-05 14:04:10 -0500

    fix persistence of datamodule

--- mlops/cifar_data_preprocessing.py
+++ mlops/cifar_data_preprocessing.py
@@ -9,8 +9,13 @@ mlflow.set_tracking_uri("sqlite:///mlruns.db")
 def download_and_transform_data(args):
     with mlflow.start_run() as mlrun:
         cifar10_dm = CIFARDataModule(args)
-        torch.save(cifar10_dm, "cifar10_dm.pkl")
-        mlflow.log_artifact("cifar10_dm.pkl", args.datamodule_path)
+        isExist = os.path.exists(args.datamodule_path)
+        if not isExist:
+            print("Creating the datamodule directory..")
+            os.makedirs(args.datamodule_path)
+        storage_path = os.path.join(args.datamodule_path, "cifar10_dm.pkl")
+        torch.save(cifar10_dm, storage_path)
+        mlflow.log_artifact(storage_path, args.datamodule_path)
         print("Data extracted, transformed and saved at ", args.datamodule_path)
 
 if __name__ == "__main__":
